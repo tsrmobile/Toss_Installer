35b5262a9d71fcaaaf39d443695c9e445ac7c00f
tsrmobile <teerayut.k@thiensurat.co.th>
2017-12-07 14:48:46 +0700

    Commit 07/12/2017

---
+++ app/src/main/java/th/co/thiensurat/toss_installer/contract/signaturepad/SignatureActivity.java
@@ -0,0 +1,256 @@
+package th.co.thiensurat.toss_installer.contract.signaturepad;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import th.co.thiensurat.toss_installer.R;
+
+public class SignatureActivity extends AppCompatActivity {
+
+    Signature signature;
+    //Button mClear, mGetSign, mCancel;
+    public static String tempDir;
+    public int count = 1;
+    public String current = null;
+    private Bitmap bitmap;
+    View view;
+    File filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND, WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        WindowManager.LayoutParams params = this.getWindow().getAttributes();
+        params.alpha = 1.0f;
+        params.dimAmount = 0.5f;
+        this.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        this.getWindow().setLayout(600,((WindowManager.LayoutParams) params).WRAP_CONTENT);
+
+        this.setFinishOnTouchOutside(false);
+        setContentView(R.layout.activity_signature);
+        setUpView();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)){
+            return false;
+        }
+        return true;
+    }
+
+    @BindView(R.id.getsign) Button buttonSave;
+    @BindView(R.id.cancel) Button buttonCancel;
+    @BindView(R.id.clear) Button buttonClear;
+    @BindView(R.id.linearLayout) LinearLayout linearLayoutPad;
+    private void setUpView() {
+        ButterKnife.bind(this);
+
+        signature = new Signature(this, null);
+        signature.setBackgroundColor(Color.WHITE);
+        linearLayoutPad.addView(signature, LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+        buttonSave.setEnabled(false);
+        view = linearLayoutPad;
+
+        buttonSave.setOnClickListener( onSign() );
+        buttonCancel.setOnClickListener( onCancel() );
+        buttonClear.setOnClickListener( onClear() );
+    }
+
+    private View.OnClickListener onClear() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               signature.clear();
+               buttonSave.setEnabled(false);
+            }
+        };
+    }
+
+    private View.OnClickListener onSign() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setDrawingCacheEnabled(true);
+                signature.save(view);
+                Bundle bundle = new Bundle();
+                bundle.putString("status", "done");
+                Intent intent = new Intent();
+                intent.putExtras(bundle);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        };
+    }
+
+    private View.OnClickListener onCancel() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("status", "cancel");
+                Intent intent = new Intent();
+                intent.putExtras(bundle);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        };
+    }
+
+
+
+    public class Signature extends View {
+
+        private static final float STROKE_WIDTH = 5f;
+        private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+        private Paint paint = new Paint();
+        private Path path = new Path();
+
+        private float lastTouchX;
+        private float lastTouchY;
+        private final RectF dirtyRect = new RectF();
+
+        public Signature(Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(STROKE_WIDTH);
+        }
+
+        public void save(View v) {
+            Log.v("log_tag", "Width: " + v.getWidth());
+            Log.v("log_tag", "Height: " + v.getHeight());
+            if(bitmap == null) {
+                bitmap =  Bitmap.createBitmap (linearLayoutPad.getWidth(), linearLayoutPad.getHeight(), Bitmap.Config.RGB_565);;
+            }
+            Canvas canvas = new Canvas(bitmap);
+            try {
+                FileOutputStream mFileOutStream = new FileOutputStream(filePath);
+
+                v.draw(canvas);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, mFileOutStream);
+                mFileOutStream.flush();
+                mFileOutStream.close();
+                String url = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "title", null);
+                Log.v("log_tag","url: " + url);
+
+            } catch(Exception e) {
+                Log.v("log_tag", e.toString());
+            }
+        }
+
+        public void clear() {
+            path.reset();
+            invalidate();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawPath(path, paint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float eventX = event.getX();
+            float eventY = event.getY();
+            buttonSave.setEnabled(true);
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    path.moveTo(eventX, eventY);
+                    lastTouchX = eventX;
+                    lastTouchY = eventY;
+                    return true;
+
+                case MotionEvent.ACTION_MOVE:
+
+                case MotionEvent.ACTION_UP:
+
+                    resetDirtyRect(eventX, eventY);
+                    int historySize = event.getHistorySize();
+                    for (int i = 0; i < historySize; i++)
+                    {
+                        float historicalX = event.getHistoricalX(i);
+                        float historicalY = event.getHistoricalY(i);
+                        expandDirtyRect(historicalX, historicalY);
+                        path.lineTo(historicalX, historicalY);
+                    }
+                    path.lineTo(eventX, eventY);
+                    break;
+
+                default:
+                    debug("Ignored touch event: " + event.toString());
+                    return false;
+            }
+
+            invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+
+            lastTouchX = eventX;
+            lastTouchY = eventY;
+
+            return true;
+        }
+
+        private void debug(String string){
+        }
+
+        private void expandDirtyRect(float historicalX, float historicalY)
+        {
+            if (historicalX < dirtyRect.left)
+            {
+                dirtyRect.left = historicalX;
+            }
+            else if (historicalX > dirtyRect.right)
+            {
+                dirtyRect.right = historicalX;
+            }
+
+            if (historicalY < dirtyRect.top)
+            {
+                dirtyRect.top = historicalY;
+            }
+            else if (historicalY > dirtyRect.bottom)
+            {
+                dirtyRect.bottom = historicalY;
+            }
+        }
+
+        private void resetDirtyRect(float eventX, float eventY)
+        {
+            dirtyRect.left = Math.min(lastTouchX, eventX);
+            dirtyRect.right = Math.max(lastTouchX, eventX);
+            dirtyRect.top = Math.min(lastTouchY, eventY);
+            dirtyRect.bottom = Math.max(lastTouchY, eventY);
+        }
+    }
+}
